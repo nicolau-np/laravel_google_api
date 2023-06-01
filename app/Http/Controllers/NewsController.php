c3a021f6e46db9202ea04da1d58b8be95c7d8da3
nicolau-np <mr1Normaliii@gmail.com>
2023-06-01 23:37:09 +0100

    trabalhandoo em noticias

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -1,10 +1,17 @@
 <?php
 
 namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Http;
 
 use Illuminate\Http\Request;
 
 class NewsController extends Controller
 {
-    //
+    public function index(){
+return view('news.index');
+    }
+
+    public function show($id){
+return view('news.show');
+    }
 }
