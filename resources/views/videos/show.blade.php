12e1b481713108153f748c856bc0528510895cb9
nicolau-np <mr1Normaliii@gmail.com>
2023-06-01 23:31:46 +0100

    update

--- resources/views/videos/show.blade.php
+++ resources/views/videos/show.blade.php
@@ -2,7 +2,8 @@
 @section('content')
 <div class="row">
     <div class="col-md-12">
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/{{ $id }}" frameborder="0" allowfullscreen></iframe>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/{{ $id }}?autoplay=1" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+
     </div>
 </div>
 @endsection
